fd860d481963f4dedd3a7e05d791b4ada0cfd257
brentnequin <brentnequin@gmail.com>
2022-11-24 23:35:57 -0600

    aww ye

--- components/Posts.vue
+++ components/Posts.vue
@@ -3,7 +3,7 @@
         <h2 class="text-3xl text-center my-8">My Blog</h2>
         <div class="m-4 flex flex-wrap justify-center items-center">
             <div v-for="page in pages" v-bind:key="page.id" class="m-4">
-                <NuxtLink :to="page.id">
+                <NuxtLink :to="{path: 'read', query: { s: page.id }}">
                     <Card 
                         :title="page.properties.Name.title[0].plain_text"
                         :created-time="page.created_time"
---
+++ pages/read/_slug.vue
@@ -0,0 +1,21 @@
+<template>
+    <main class="container my-24">
+        {{ slug }}
+    </main>
+</template>
+
+<script>
+export default {
+
+    data() {
+        return {
+
+        }
+    },
+
+    created() {
+        console.log(this.$route.query.s);
+        this.slug = this.$route.query.s
+    }
+}
+</script>
